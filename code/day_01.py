c8987adfd550eec27b5118deaa1b4c1821bf2409
Martins Bruveris <martins.bruveris@gmail.com>
2021-12-31 14:47:04 +0000

    Simplified day 1.

--- code/day_01.py
+++ code/day_01.py
@@ -5,10 +5,16 @@ import click
 
 @click.command()
 @click.argument("filename")
-def main(filename):
+@click.option("--part", type=click.Choice(["a", "b"]))
+def main(filename, part):
     filename = Path(filename)
     depths = filename.read_text().split("\n")
     depths = [int(depth) for depth in depths]
+
+    window_size = 1 if part == "a" else 3
+    depths = [
+        sum(depths[j : j + window_size]) for j in range(len(depths) - window_size + 1)
+    ]
 
     pairs = zip(depths[:-1], depths[1:])
     nb_increases = sum(p[1] > p[0] for p in pairs)
