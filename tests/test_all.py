d14460f5e662003f46cfdf1422d85b1d94070e46
Martins Bruveris <martins.bruveris@gmail.com>
2022-12-01 11:16:16 +0000

    Continuing refactoring.

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,14 +6,14 @@ from click.testing import CliRunner
 
 
 def solved_puzzles():
-    solved = {"2022": [1]}
+    solved = {"2021": [1], "2022": [1]}
     solved_list = [(year, day) for year, day_list in solved.items() for day in day_list]
     return solved_list
 
 
-@pytest.mark.parametrize("year, day", solved_puzzles())
-@pytest.mark.parametrize("part", ["a", "b"])
 @pytest.mark.parametrize("test", [True, False])
+@pytest.mark.parametrize("part", ["a", "b"])
+@pytest.mark.parametrize("year, day", solved_puzzles())
 def test_one_puzzle(year: int, day: int, part: str, test: bool):
     mod = importlib.import_module(f"aoc.aoc_{year}.day_{day:02}")
     input_file = f"data/{year}/day_{day:02}" + ("_test" if test else "") + ".txt"
