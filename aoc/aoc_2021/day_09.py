5ba65bada7d0db55ed8b5c6157352026037ae455
Martins Bruveris <martins.bruveris@gmail.com>
2022-12-01 22:22:43 +0000

    Added 2021, day 9.

---
+++ aoc/aoc_2021/day_09.py
@@ -0,0 +1,63 @@
+from math import prod
+from pathlib import Path
+
+import click
+import numpy as np
+
+
+def basin_size(arr, x, y):
+    member = np.zeros_like(arr)  # Basin membership
+    member[x, y] = 1
+
+    for _ in range(9):  # Basins can never extend more than 10 squares from origin
+        member[1:-1, 1:-1] = np.maximum(
+            member[1:-1, 1:-1], (arr[1:-1, 1:-1] > arr[2:, 1:-1]) * member[2:, 1:-1]
+        )
+        member[1:-1, 1:-1] = np.maximum(
+            member[1:-1, 1:-1], (arr[1:-1, 1:-1] > arr[:-2, 1:-1]) * member[:-2, 1:-1]
+        )
+        member[1:-1, 1:-1] = np.maximum(
+            member[1:-1, 1:-1], (arr[1:-1, 1:-1] > arr[1:-1, 2:]) * member[1:-1, 2:]
+        )
+        member[1:-1, 1:-1] = np.maximum(
+            member[1:-1, 1:-1], (arr[1:-1, 1:-1] > arr[1:-1, :-2]) * member[1:-1, :-2]
+        )
+        member[arr == 9] = 0  # 9s are never part of basins
+    return np.sum(member)
+
+
+@click.command()
+@click.argument("filename")
+@click.option("--part", type=click.Choice(["a", "b"]))
+def main(filename, part):
+    filename = Path(filename)
+    heights = filename.read_text().split("\n")
+    heights = [[int(height) for height in line] for line in heights]
+    heights = np.asarray(heights)
+
+    arr = np.pad(heights, pad_width=10, mode="constant", constant_values=9)
+    heights = arr[1:-1, 1:-1]
+    left = arr[:-2, 1:-1]
+    right = arr[2:, 1:-1]
+    top = arr[1:-1, :-2]
+    bottom = arr[1:-1, 2:]
+    min_neighbour = np.min([left, right, top, bottom], axis=0)
+    is_low = heights < min_neighbour
+
+    if part == "a":
+        low_heights = heights[is_low]
+        result = np.sum(1 + low_heights)
+    else:
+        indices = np.argwhere(is_low) + 1  # +1 to add the margin back in
+        basins = [
+            basin_size(arr[x - 10 : x + 10, y - 10 : y + 10], 10, 10)
+            for x, y in indices
+        ]
+        basins.sort(reverse=True)
+        result = prod(basins[:3])
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_all.py
+++ tests/test_all.py
@@ -6,7 +6,7 @@ from click.testing import CliRunner
 
 
 def solved_puzzles(year=None):
-    solved = {2015: [1], 2021: [1, 4, 18], 2022: [1]}
+    solved = {2015: [1], 2021: [1, 4, 9], 2022: [1]}
     if year is not None:
         solved_list = [(year, day) for day in solved[year]]
     else:
@@ -18,7 +18,7 @@ def solved_puzzles(year=None):
 
 @pytest.mark.parametrize("test", [True, False])
 @pytest.mark.parametrize("part", ["a", "b"])
-@pytest.mark.parametrize("year, day", solved_puzzles(year=2015))
+@pytest.mark.parametrize("year, day", solved_puzzles())
 def test_one_puzzle(year: int, day: int, part: str, test: bool):
     mod = importlib.import_module(f"aoc.aoc_{year}.day_{day:02}")
     input_file = f"data/{year}/day_{day:02}" + ("_test" if test else "") + ".txt"
