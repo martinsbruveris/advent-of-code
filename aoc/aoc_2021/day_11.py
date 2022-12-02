3e84e741434eb8e2e8ff89d17734cd6e11cfa5f0
Martins Bruveris <martins.bruveris@gmail.com>
2022-12-02 18:15:37 +0000

    Added 2021, day 11.

---
+++ aoc/aoc_2021/day_11.py
@@ -0,0 +1,50 @@
+from itertools import count
+from pathlib import Path
+
+import click
+import numpy as np
+
+
+def step(arr):
+    arr += 1
+    xm, ym = arr.shape
+
+    while np.sum((9 < arr) & (arr < 20)) > 0:
+        indices = np.argwhere((9 < arr) & (arr < 20))
+        arr[arr > 9] = 20
+        for x, y in indices:
+            arr[
+                max(x - 1, 0) : min(x + 2, xm),
+                max(y - 1, 0) : min(y + 2, ym),
+            ] += 1
+    flashes = np.sum(arr > 9)
+    arr[arr > 9] = 0
+    return flashes, arr
+
+
+@click.command()
+@click.argument("filename")
+@click.option("--part", type=click.Choice(["a", "b"]))
+def main(filename, part):
+    filename = Path(filename)
+    energies = filename.read_text().split("\n")
+    energies = [[int(e) for e in line] for line in energies]
+    energies = np.asarray(energies)
+
+    if part == "a":
+        result = 0
+        for _ in range(100):
+            num, energies = step(energies)
+            result += num
+    else:
+        for j in count(1):
+            _, energies = step(energies)
+            if np.sum(energies) == 0:
+                break
+        result = j
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
